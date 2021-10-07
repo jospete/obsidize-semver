e9a0f469010dc9278fd568a1784c2025a0ca10ad
Josh Noel <obsidize@gmail.com>
2021-10-07 17:02:03 -0500

    misc test updates

--- tests/core/semver-comparator.spec.ts
+++ tests/core/semver-comparator.spec.ts
@@ -1,9 +1,48 @@
-import { SemverComparator } from '../../src';
+import { SemverComparator, Semver } from '../../src';
 
 describe('SemverComparator', () => {
 
 	it('does not explode when given bad input', () => {
 		const comparator = new SemverComparator(() => { throw new Error('should not get here'); });
 		expect(comparator.compare('test1', 'test2')).toBe(comparator.compareFailureResult);
+	});
+
+	describe('lessThan', () => {
+
+		it('uses the default value when one is given', () => {
+			expect(Semver.lessThan(null, null)).toBe(false);
+			expect(Semver.lessThan(null, null, true)).toBe(true);
+		});
+	});
+
+	describe('lessThanOrEqual', () => {
+
+		it('uses the default value when one is given', () => {
+			expect(Semver.lessThanOrEqual(null, null)).toBe(false);
+			expect(Semver.lessThanOrEqual(null, null, true)).toBe(true);
+		});
+	});
+
+	describe('greaterThanOrEqual', () => {
+
+		it('uses the default value when one is given', () => {
+			expect(Semver.greaterThanOrEqual(null, null)).toBe(false);
+			expect(Semver.greaterThanOrEqual(null, null, true)).toBe(true);
+		});
+	});
+
+	describe('equals', () => {
+
+		it('uses the default value when one is given', () => {
+			expect(Semver.equals(null, null)).toBe(false);
+			expect(Semver.equals(null, null, true)).toBe(true);
+		});
+
+		it('works as expected', () => {
+			expect(Semver.equals('1.0.0', '1')).toBe(true);
+			expect(Semver.equals('1.0.0', '1.0')).toBe(true);
+			expect(Semver.equals('1.0.0', '1.0.0')).toBe(true);
+			expect(Semver.equals('1.0.0', '1.0.0.1')).toBe(false);
+		});
 	});
 });
--- tests/example-usage.spec.ts
+++ tests/example-usage.spec.ts
@@ -1,8 +1,16 @@
-import { Semver, SemverContext } from '../src';
+import { Semver, SemverContext, SemverUtility } from '../src';
 
 describe('General Usage', () => {
 
 	it('works as advertised', () => {
+		expect(Semver.lessThan('1.0.0', '1.0.1')).toBe(true);
+		expect(Semver.greaterThan('1.0.0', '0.999.9999')).toBe(true);
+		expect(Semver.lessThanOrEqual('1.0.0', '0.999.9999', true)).toBe(false);
+		expect(Semver.lessThanOrEqual('1.0.0', null, true)).toBe(true);
+		expect(Semver.compare(null, null)).toBe(SemverUtility.COMPARE_FAILURE_DEFAULT);
+	});
+
+	it('can create context instances to make repetitive comparisons more simple', () => {
 		const version = new SemverContext(() => '1.2.3');
 		expect(version.greaterThan('1.0.0')).toBe(true);
 		expect(version.greaterThanOrEqual('1.2.3')).toBe(true);
